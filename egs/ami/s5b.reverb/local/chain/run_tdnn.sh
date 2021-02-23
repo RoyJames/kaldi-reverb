ce32c80ec06c5467d511b64501709ac4c9ac0e24
royjames <zhy@cs.umd.edu>
2021-02-23 18:58:26 -0500

    ami reverb train recipe

--- egs/ami/s5b.reverb/local/chain/run_tdnn.sh
+++ egs/ami/s5b.reverb/local/chain/run_tdnn.sh
@@ -246,7 +246,7 @@ if [ $stage -le 16 ]; then
     --trainer.frames-per-iter 1500000 \
     --trainer.num-epochs $num_epochs \
     --trainer.optimization.num-jobs-initial 2 \
-    --trainer.optimization.num-jobs-final 12 \
+    --trainer.optimization.num-jobs-final 2 \
     --trainer.optimization.initial-effective-lrate 0.001 \
     --trainer.optimization.final-effective-lrate 0.0001 \
     --trainer.max-param-change 2.0 \
