7ed84453f3c822c702f276bc38f33fa4eee4452f
royjames <zhy@cs.umd.edu>
2021-03-08 16:45:19 -0500

    include sdm results

--- egs/ami/s5b/local/get_results.sh
+++ egs/ami/s5b/local/get_results.sh
@@ -1,1 +1,1 @@
-for d in exp/*/chain_cleaned/tdnn1j_sp_bi/decode*; do grep Sum $d/*sc*/*ys | utils/best_wer.sh; done
+for d in exp/*/chain_cleaned/tdnn*/decode*; do grep Sum $d/*sc*/*ys | utils/best_wer.sh; done
