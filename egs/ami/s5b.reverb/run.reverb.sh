605d41ddb1d31d19ced3308091a413f4ac5032aa
royjames <zhy@cs.umd.edu>
2021-03-15 02:18:21 -0400

    one pass recipe

--- egs/ami/s5b.reverb/run.reverb.sh
+++ egs/ami/s5b.reverb/run.reverb.sh
@@ -14,10 +14,11 @@
 # ./run.sh --mic sdm1
 # ./run.sh --mic mdm8
 mic=ihm
+ami=/scratch/zhy/ASR_Data/AMI
 
 # Train systems,
 nj=60 # number of parallel jobs,
-stage=10
+stage=1
 . utils/parse_options.sh
 
 base_mic=$(echo $mic | sed 's/[0-9]//g') # sdm, ihm or mdm
@@ -26,19 +27,63 @@ nmics=$(echo $mic | sed 's/[a-z]//g') # e.g. 8 for mdm8.
 set -euo pipefail
 
 # Path where AMI gets downloaded (or where locally available):
-AMI_DIR=/scratch/zhy/ASR_Data/AMI # Default,
+AMI_DIR=$ami # reverberated AMI dir
 case $(hostname -d) in
   fit.vutbr.cz) AMI_DIR=/mnt/matylda5/iveselyk/KALDI_AMI_WAV ;; # BUT,
   clsp.jhu.edu) AMI_DIR=/export/corpora4/ami/amicorpus ;; # JHU,
   cstr.ed.ac.uk) AMI_DIR= ;; # Edinburgh,
 esac
-AMI_DIR_DEVEV=/mnt/matylda5/iveselyk/KALDI_AMI_WAV
-AMI_DIR_TRAIN=---  ### set path here to your reverberated data ###
 
 [ ! -r data/local/lm/final_lm ] && echo "Please, run 'run_prepare_shared.sh' first!" && exit 1
 final_lm=`cat data/local/lm/final_lm`
 LM=$final_lm.pr1-7
 
+if [ "$base_mic" == "mdm" ]; then
+  PROCESSED_AMI_DIR=$AMI_DIR/beamformed
+  if [ $stage -le 1 ]; then
+    # for MDM data, do beamforming
+    ! hash BeamformIt && echo "Missing BeamformIt, run 'cd ../../../tools/; extras/install_beamformit.sh; cd -;'" && exit 1
+    local/ami_beamform.sh --cmd "$train_cmd" --nj 20 $nmics $AMI_DIR $PROCESSED_AMI_DIR
+  fi
+else
+  PROCESSED_AMI_DIR=$AMI_DIR
+fi
+
+# Prepare original data directories data/ihm/train_orig, etc.
+if [ $stage -le 2 ]; then
+  local/ami_${base_mic}_data_prep.sh $PROCESSED_AMI_DIR $mic
+  local/ami_${base_mic}_scoring_data_prep.sh $PROCESSED_AMI_DIR $mic dev
+  local/ami_${base_mic}_scoring_data_prep.sh $PROCESSED_AMI_DIR $mic eval
+fi
+
+if [ $stage -le 3 ]; then
+  for dset in train dev eval; do
+    # this splits up the speakers (which for sdm and mdm just correspond
+    # to recordings) into 30-second chunks.  It's like a very brain-dead form
+    # of diarization; we can later replace it with 'real' diarization.
+    seconds_per_spk_max=30
+    [ "$mic" == "ihm" ] && seconds_per_spk_max=120  # speaker info for ihm is real,
+                                                    # so organize into much bigger chunks.
+
+    # Note: the 30 on the next line should have been $seconds_per_spk_max
+    # (thanks: Pavel Denisov.  This is a bug but before fixing it we'd have to
+    # test the WER impact.  I suspect it will be quite small and maybe hard to
+    # measure consistently.
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 30 \
+      data/$mic/${dset}_orig data/$mic/$dset
+  done
+fi
+
+# Feature extraction,
+if [ $stage -le 4 ]; then
+  for dset in train dev eval; do
+    steps/make_mfcc.sh --nj 15 --cmd "$train_cmd" data/$mic/$dset
+    steps/compute_cmvn_stats.sh data/$mic/$dset
+    utils/fix_data_dir.sh data/$mic/$dset
+  done
+fi
+
+# Modified pipeline begins
 if [ $stage -le 10 ]; then
   echo "== Stage 10 =="
   # The following script cleans the data and produces cleaned data
