108620053130b72c41c77e2d2a2154b4b87d2472
Zhenyu Tang <zhy@cs.umd.edu>
2022-03-04 20:36:19 -0500

    fix syntax

--- egs/reverb/s5/local/get_results.sh
+++ egs/reverb/s5/local/get_results.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "TDNN RESULTs:"
-echo "exp/chain_{1}/tdnn1a_sp/decode_test_tg_5k_dt*"
-cat exp/chain_{1}/tdnn1a_sp/decode_test_tg_5k_dt*/scoring_kaldi/best_wer_*
+echo "exp/chain_$1/tdnn1a_sp/decode_test_tg_5k_dt*"
+cat exp/chain_$1/tdnn1a_sp/decode_test_tg_5k_dt*/scoring_kaldi/best_wer_*
 echo ""
-echo "exp/chain_{1}/tdnn1a_sp/decode_test_tg_5k_et*"
-cat exp/chain_{1}/tdnn1a_sp/decode_test_tg_5k_et*/scoring_kaldi/best_wer_*
+echo "exp/chain_$1/tdnn1a_sp/decode_test_tg_5k_et*"
+cat exp/chain_$1/tdnn1a_sp/decode_test_tg_5k_et*/scoring_kaldi/best_wer_*
